0bdc3a6da14cd6c8da04bc224918a16793ccf611
Abby Henríquez Tejera <paradoja@gmail.com>
2012-12-20 11:24:29 +0000

    Change log to topic.

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -360,8 +360,8 @@ ERROR
 
   # runs bundler to install the dependencies
   def build_bundler
-    log(Dir.pwd)
-    log(ENV['BUNDLE_GEMFILE'])
+    topic(Dir.pwd)
+    topic(ENV['BUNDLE_GEMFILE'])
     log("bundle") do
       bundle_without = ENV["BUNDLE_WITHOUT"] || "development:test"
       bundle_command = "bundle install --without #{bundle_without} --path vendor/bundle --binstubs vendor/bundle/bin"
